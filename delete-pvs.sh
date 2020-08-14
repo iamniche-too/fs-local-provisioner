2bb252acff53469e9ab0d95145b2385965505cec
iamniche-too <nicholas.oliver.hemley@gmail.com>
2020-08-14 21:32:24 +0100

    Delete Released and Available PVs.

--- delete-pvs.sh
+++ delete-pvs.sh
@@ -2,5 +2,8 @@
 source ./export-gcp-credentials.sh
 ./generate-cluster-connection-yaml.sh gke-kafka-cluster 
 
-# delete the Released PVs 
+# delete any Released PVs 
 kubectl get pv --kubeconfig ./kubeconfig.yaml | grep Released | awk '$1 {print$1}' | while read vol; do kubectl delete pv/${vol} --kubeconfig ./kubeconfig.yaml; done
+
+# delete any Available PVs
+kubectl get pv --kubeconfig ./kubeconfig.yaml | grep Available | awk '$1 {print$1}' | while read vol; do kubectl delete pv/${vol} --kubeconfig ./kubeconfig.yaml; done
