3946366a8023f68a0b7fab6af0da706f3f702c9b
iamniche-too <nicholas.oliver.hemley@gmail.com>
2020-08-14 14:01:44 +0100

    Added -test suffix to all test scripts.

--- get-ds-test.sh
+++ get-ds-test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 source ./export-gcp-credentials.sh
-./generate-cluster-connection-yaml.sh gke-kafka-cluster
+./generate-cluster-connection-yaml.sh test-local-ssd-cluster
 
 kubectl get ds --kubeconfig ./kubeconfig.yaml
